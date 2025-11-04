3af0c1c54827974a2a41d06bd9276235ae9cc533
TabularElf <tabularelf@gmail.com>
2025-11-04 12:38:08 +1100

    Updated config (whoops)

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 	name: 'Lexicon',
 	description: 'Localization library for GameMaker',
-	latestVersion: '3.0.6',
-	otherVersions: ['4.0.0','2.1.3'],
+	latestVersion: '4.0.2',
+	otherVersions: ['3.0.6','2.1.3'],
 	favicon: 'assets/favicon.ico',
 };
